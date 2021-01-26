d52e35ab1d1f05c68c910abd4e206c54c211989f
Josiel Sousa <josiel.psousa@gmail.com>
2021-01-26 14:35:57 -0300

    chore: modify account exception constructors.

--- src/main/java/br/com/caj/domain/usecase/exception/AccountException.java
+++ src/main/java/br/com/caj/domain/usecase/exception/AccountException.java
@@ -7,4 +7,20 @@ public class AccountException extends RuntimeException {
 
   private static final long serialVersionUID = -2696604476854706368L;
 
+  /**
+   * Default constructor.
+   */
+  public AccountException() {
+
+  }
+
+  /**
+   * Default constructor with message.
+   * 
+   * @param message
+   */
+  public AccountException(String message) {
+    super(message);
+  }
+
 }
